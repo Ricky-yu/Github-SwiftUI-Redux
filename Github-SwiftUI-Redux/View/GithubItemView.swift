7e0f3f4124a3a8d8e2011a10a74f82464e499942
Ricky-yu <ts00484515@gmail.com>
2022-09-03 23:24:04 +0900

    add GithubItemView

---
+++ Github-SwiftUI-Redux/View/GithubItemView.swift
@@ -0,0 +1,19 @@
+//
+//  GithubItemView.swift
+//  Github-SwiftUI-Redux
+//
+//  Created by chensinyu on 2022/09/03.
+//
+
+import SwiftUI
+
+struct GithubItemView: View {
+
+
+}
+
+struct GithubItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+    }
+}
