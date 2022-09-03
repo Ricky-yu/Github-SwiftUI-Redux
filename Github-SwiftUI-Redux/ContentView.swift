cf9cc0ac87e0b3acae3b861881801b15cec6556e
Ricky-yu <ts00484515@gmail.com>
2022-09-03 23:23:10 +0900

    create MainView for search GithubRepo

--- Github-SwiftUI-Redux/ContentView.swift
+++ Github-SwiftUI-Redux/ContentView.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var store: Store
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                List(store.appState.items) { item in
+                    GithubItemView(repo: item)
+                }
+                if store.appState.isLoading {
+                    ProgressView()
+                        .scaleEffect(1.5, anchor: .center)
+                        .tint(.cyan)
+                }
+            }
+            .navigationTitle(Text("Github Repository"))
+        }
+        .searchable(text: $store.appState.searchText)
+        .onSubmit(of: .search, {
+
+        })
+        .alert(isPresented: $store.appState.isShowAlert) {
+            Alert(
+                title: Text("エラー"),
+                message: Text(store.appState.alertMessage),
+                dismissButton: .default(Text("OK")))
+        }
     }
 }
 
